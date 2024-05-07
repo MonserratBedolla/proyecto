ae03f2f25572e680e00a589f91520fce7a34e06d
MonserratBedolla <99926583+MonserratBedolla@users.noreply.github.com>
2024-05-06 20:09:11 -0600

    Add files via upload

---
+++ Jorge.js
@@ -0,0 +1,99 @@
+var player, back;
+var points = 0;
+var backImg;
+var leftPlayerImg;
+var rightPlayerImg;
+var gameState= "START";
+var baseImg, baseGroup;
+var goodImg, goodThingsGroup;
+var badImg, badThingsGroup;
+
+//Función para cargar imágenes y animaciones
+function preload() {
+    leftPlayerImg = loadAnimation("rabbitLeft.png");
+    baseImg = loasImage ("base.png");
+}
+//Función para declarar Sprites y grupos
+function setup() {
+  createCanvas(450,800);
+  player = createSprite(225,520,20,20);
+}
+//Función para dibujar los Sprites y establecer reglas del juego
+function draw() {
+  background(220);
+  drawSprites();
+  
+  if(keyDown("right_arrow")){
+    player.x = player.x + 3
+  }
+  
+  if(keyDown("left_arrow")){
+    player.x = player.x - 3
+  }
+  
+  if(keyDown("up_arrow")){
+    player.velocityY  = -4
+  }
+  
+ 
+  createBases();
+  //Puntuación 
+ 
+  //Inicio del juego
+  if(gameState==="START" && keydown("UP_ARROW")){
+      
+    gameState = "PLAY";
+    //Velocidad y cambio de estado 
+    
+     }
+  
+  if(gameState==="PLAY"){
+    //Fondo infinito
+   
+    //gravedad
+     player.velocityY = player.velocityY + 0.2;
+     
+    //Mover personaje con las flechas 
+    
+    //crear bases y hacer que el personaje quede sobre ellas
+    
+    //Aumentar puntos
+    
+    //crear Cosas Malas 
+      
+    //Cambiar a estado GAMEOVER
+    
+  }
+  
+  //Estado GAMEOVER 
+  
+  if(gameState==="GAMEOVER"){
+     }
+  
+
+}
+
+//Función para crear bases 
+function createBases(){
+   if(frameCount % 100 === 0){
+     var base = createSprite(random(50,400),0,70,20);
+     base.velocityY = 2;
+     base.addImage(baseImage);
+     base.scale = 0.3
+   }
+}
+
+//Función para crear Cosas Malas 
+
+function createBadThings(){
+  var velo = 3;
+  if(frameCount % 75 === 0){
+     
+}
+}
+
+//Función para eliminar CosasBuenas
+function removeGoodThings(sprite,goodThingsGroup ){
+ 
+}
+
